6da057670cb30a5a6c9698737f785524de0d2a64
Alena Pavlenkova <A.P.Forever@yandex.ru>
2023-12-16 14:07:24 +0400

    learning JS

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-function summa(arr) {
-    var sum = 0;
-    for(var i=0; i<arr.length; i++){
-        sum+=arr[i];
-    }
-    return sum;
+var text = document.getElementById('text');
+
+console.log();
+
+//var spans = document.getElementsByTagName ('span');
+var spans = document.getElementsByClassName('simple-text');
+for(var i = 0; i< spans.length; i++){
+    console.log(spans[i].innerHTML);
 }
-
-var array = [6, 8, 1];
-var array_2 = [6, 8, 1, 7];
-var array_3 = [6, 8, 1, 90];
-
-var res = summa(array);
-var res_2 = summa(array_2);
-var res_3 = summa(array_3);
-
-console.log("Результат: "+ res);
-console.log("Результат: "+ res_2);
-console.log("Результат: "+ res_3);
-
-console.log("привет, мир!!");
